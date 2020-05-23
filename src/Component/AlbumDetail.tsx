df1949c56e65e4b2fc180b0c5b2b6d5a132484ce
Miles Loveday <miles.loveday@gmail.com>
2020-05-23 21:31:37 +0100

    album details page

--- src/Component/AlbumDetail.tsx
+++ src/Component/AlbumDetail.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import * as Redux from 'react-redux';
-import {match} from 'react-router-dom';
+import {Link, match, useLocation} from 'react-router-dom';
+import * as moment from 'moment';
 import {AppState} from "../Store/store";
 import {getAlbum} from "../Store/AlbumState";
 
 export const AlbumDetail: React.FC<{match: match<{id: string}>}> = props => {
     const albumState = Redux.useSelector((state: AppState) => state.albums);
     const dispatch = Redux.useDispatch();
+    const location = useLocation();
 
     const album = albumState.albums.find(album => album.id === props.match.params.id) ?? (albumState.album?.id === props.match.params.id ? albumState.album : undefined);
 
@@ -17,8 +19,16 @@ export const AlbumDetail: React.FC<{match: match<{id: string}>}> = props => {
         return <div>Album not found</div>;
     }
 
+    const image = album.images.length > 0 ? album.images[0] : undefined;
+
     return <div>
-        <h2>Album!</h2>
-        <div>{album.name}</div>
+        <Link to={`/${location.search}`}>&lt;&lt; back to list</Link>
+        <h2>{album.name}</h2>
+        <img src={image.url} alt={`Album artwork`}/>
+        <h3>Artists</h3>
+        <ul>{album.artists.map(artist => <li key={artist.id}>{artist.name}</li>)}</ul>
+        <h3>Other information</h3>
+        <div>Released on {moment(album.release_date).format('dddd, MMMM Do YYYY')}</div>
+        <div>{album.total_tracks} tracks</div>
     </div>
 };
