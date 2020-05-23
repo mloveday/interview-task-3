37810cd57acaf541f2575baaee5554918466b580
Miles Loveday <miles.loveday@gmail.com>
2020-05-23 14:24:42 +0100

    Reduce the margin on the nav down significantly

--- src/Component/NavBar.tsx
+++ src/Component/NavBar.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+const CompactNav = styled.nav`
+  margin-bottom: 1rem;
+`;
+
 export const NavBar: React.FC = props => {
-    return <nav>
+    return <CompactNav>
         <ul>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/credentials'>Client credentials</Link></li>
         </ul>
-    </nav>
+    </CompactNav>
 };
