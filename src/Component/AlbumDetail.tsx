61856d593c6fe26aa81f5509c0cd51db0bdf11d0
Miles Loveday <miles.loveday@gmail.com>
2020-05-23 19:08:17 +0100

    Fix album id type validation in AlbumDetail component

--- src/Component/AlbumDetail.tsx
+++ src/Component/AlbumDetail.tsx
@@ -4,12 +4,8 @@ import {match} from 'react-router-dom';
 import {AppState} from "../Store/store";
 
 export const AlbumDetail: React.FC<{match: match<{id: string}>}> = props => {
-    const albums = Redux.useSelector((state: AppState) => state.albums);
-    const id = parseInt(props.match.params.id, 10);
-    if (!Number.isInteger(id)) {
-        return <div>Album id is not valid</div>
-    }
-    const album = albums.find(album => album.id === id);
+    const albumState = Redux.useSelector((state: AppState) => state.albums);
+    const album = albumState.albums.find(album => album.id === props.match.params.id);
 
     if (album === undefined) {
         return <div>Album not found</div>;
