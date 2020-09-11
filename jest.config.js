e2baedc67cab67250996b694ce75cc1df9317baa
Miles Loveday <miles.loveday@gmail.com>
2020-09-11 06:32:20 +0100

    Ignore root index & store from test coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
   setupFiles: ["jest-localstorage-mock", "./test/setupJest.ts"],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'src/index.tsx',
+    'src/Store/store.ts',
+  ]
 };
