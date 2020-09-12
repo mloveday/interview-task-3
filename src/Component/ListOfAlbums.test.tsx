d2ec0039d6853eff7735c66ef236ab2f2e26e52c
Miles Loveday <miles.loveday@gmail.com>
2020-09-12 18:32:41 +0100

    Test ListOfAlbums component

---
+++ src/Component/ListOfAlbums.test.tsx
@@ -0,0 +1,53 @@
+import 'jsdom-global/register';
+import * as React from 'react';
+import * as Enzyme from 'enzyme';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import {ListOfAlbums} from "./ListOfAlbums";
+import thunk from 'redux-thunk';
+import {AlbumInList} from "./AlbumInList";
+
+describe('ListOfAlbums component', () => {
+
+  const createAlbum = (id, artists, images) => ({
+    id,
+    name: 'foobar',
+    artists: artists,
+    release_date: '2020-09-08',
+    total_tracks: 5,
+    images: images,
+  })
+
+  const createState = (albums) => ({
+    albums: {
+      fetchState: 'loaded',
+      albums,
+    }
+  })
+
+  const render = (mockStore) => Enzyme.mount(
+    <Provider store={mockStore}>
+      <BrowserRouter>
+        <ListOfAlbums />
+      </BrowserRouter>
+    </Provider>
+  );
+
+  test('renders list from state', () => {
+    const albums = [
+      createAlbum('id-1', [], []),
+      createAlbum('id-2', [], []),
+      createAlbum('id-3', [], []),
+    ];
+    const mockState = createState(albums);
+    const mockStore = configureStore([thunk])(mockState);
+
+    const result = render(mockStore);
+
+
+    albums.forEach(album => {
+      expect(result.find(AlbumInList).find({album: album})).toHaveLength(1);
+    })
+  });
+});
